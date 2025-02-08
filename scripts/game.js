9ae5e40a10a4ac504e99294f006277fad3af9d30
Fabian Kraus <108660093+DerFoB@users.noreply.github.com>
2025-02-08 20:22:40 +0100

    export popup

--- scripts/game.js
+++ scripts/game.js
@@ -5,7 +5,7 @@ import { Shot } from './objects/shots.js';
 import { createExplosion } from './objects/explosion.js';
 import { keyState } from './functionality/inputHandler.js';
 import { collision, deleteObjectFromArray, drawIfVisible } from './functionality/eventHandler.js';
-import { showRestartPopup } from './functionality/popup.js';
+import { endGamePopup } from './functionality/popup.js';
 
 /// Settings
 const rocketSpeed = 12;
@@ -51,47 +51,7 @@ function startGame(){
 
 function endGame(){
     gameRunning = false;
-    setTimeout(() => {
-        let highscorePlayer;
-    
-        if (localStorage.getItem("highscore") !== null) {
-            highscorePlayer = JSON.parse(localStorage.getItem("highscore"));
-        } else {
-            highscorePlayer = { name: "unknown", score: -99 };
-        }
-    
-        let modal = document.getElementById("gameModal");
-        let modalTitle = document.getElementById("modalTitle");
-        let modalMessage = document.getElementById("modalMessage");
-        let inputField = document.getElementById("playerNameInput");
-        let modalButton = document.getElementById("modalButton");
-        let overlay = document.getElementById("overlay");
-
-        overlay.style.display = "block"; // blur background
-    
-        // if new highscore
-        if (score > highscorePlayer.score) {
-            modalTitle.innerText = "New Highscore!";
-            modalMessage.innerText = `Your highscore is: ${score}\nEnter your name:`;
-            inputField.style.display = "block"; // show input
-            modalButton.innerText = "Save";
-    
-            modal.style.display = "block"; // show Popup
-    
-            modalButton.onclick = function () {
-                let playerName = inputField.value.trim() || "unknown";
-    
-                // save highscore
-                highscorePlayer.name = playerName;
-                highscorePlayer.score = score;
-                localStorage.setItem("highscore", JSON.stringify(highscorePlayer));
-    
-                showRestartPopup(`New Highscore!!!\nYour highscore: ${score} by ${playerName}`);
-            };
-        } else {
-            showRestartPopup(`Your score: ${score}\nLocal highscore: ${highscorePlayer.score} by ${highscorePlayer.name}`);
-        }
-    }, 200);
+    endGamePopup(score);
 }
 
 
